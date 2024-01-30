5bd58eae51ae70ecf8c95e1255cb64bb5d582e6c
Docenko Pasha <pashadocenko@gmail.com>
2024-01-30 22:16:37 +0300

    Plugins were added.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
-module.exports =  (env) => {
-   return {
-       mode : env.mode ?? "development",
-       entry: path.resolve(__dirname, "src", "index.js"),
-       output: {
-           path: path.resolve(__dirname, "build"),
-           filename: "bundle.[name].[contenthash].js",
-           clean: true
-       }
-   }
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? "development",
+        entry: path.resolve(__dirname, "src", "index.js"),
+        output: {
+            path: path.resolve(__dirname, "build"),
+            filename: "bundle.[name].[contenthash].js",
+            clean: true
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, "public", "index.html") }),
+            new webpack.ProgressPlugin()
+        ],
+
+    };
 };
