bf547265164bbee22dacc2e59a42e5061b8ebb40
Docenko Pasha <pashadocenko@gmail.com>
2024-02-03 00:01:45 +0300

    mend a mistake

--- webpack.config.ts
+++ webpack.config.ts
@@ -13,7 +13,7 @@ interface EnvVariables {
 export default (env: EnvVariables) => {
     const paths: BuildPaths = {
         output: path.resolve(__dirname, "build"),
-        entry: path.resolve(__dirname, "src", "index.ts"),
+        entry: path.resolve(__dirname, "src", "index.tsx"),
         html: path.resolve(__dirname, "public", "index.html"),
     };
 
